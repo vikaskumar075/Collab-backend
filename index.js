117da28d3ea7d6d4faebc21ded7b254810abc7a4
vikas <vikaskumar638727@gmail.com>
2025-07-23 21:18:53 +0530

    correct cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://collab-frontend-4nja.vercel.app/',
+    origin: 'https://collab-frontend-4nja.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
